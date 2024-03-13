3803f81e2042b445352f6263ab14388bdcc24ec9
Italo <italo.sa01@etec.sp.gov.br>
2024-03-13 20:16:05 -0300

    Aula dia 13-05

---
+++ 13-05.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="acao.php" method="POST">
+      <pre> 
+        <label for= "">Nome: </label>
+        <input type="text" name ="nome" id="">
+
+        <label for=""> Email: </label>
+        <input type = "email" name = "email" id="">
+
+    <input type ="submit" value ="enviar"> <input type= "reset"  value= "Limpar">
+</pre>
+        
+    </form>
+
+</body>
+</html>
---
+++ acao.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    if($_POST['email'] =="admin@email.com"){?>
+    bem vindo <?php echo $_POST ['nome'];?>
+    Seu email é <?php echo ['email'];?>
+    <?php
+    }
+    ?>
+    
+    
+    
+</body>
+</html>
